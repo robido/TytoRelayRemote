704f87d3e6e6f4f655538ed6bcb3a900e6317e99
robido <dominic@robipro.com>
2014-09-11 17:34:48 -0400

    Added multiwii code.

---
+++ Firmware/README.md
@@ -0,0 +1,6 @@
+multiwii-firmware
+=================
+
+Firmware - GitHub mirror of the official SVN multiwii project
+
+For research on flybarless stabilization and optical flow.
---
+++ Firmware/Visual Micro/.Firmware.vsarduino.h
@@ -0,0 +1,70 @@
+/* 
+	Editor: http://www.visualmicro.com
+	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	
+	Hardware: Arduino Mega 2560 or Mega ADK, Platform=avr, Package=arduino
+*/
+
+#ifndef _VSARDUINO_H_
+#define _VSARDUINO_H_
+#define __AVR_ATmega2560__
+#define ARDUINO 105
+#define ARDUINO_MAIN
+#define __AVR__
+#define __avr__
+#define F_CPU 16000000L
+#define __cplusplus
+#define __inline__
+#define __asm__(x)
+#define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
+#define __inline__
+#define __asm__ 
+#define __volatile__
+
+#define __builtin_va_list
+#define __builtin_va_start
+#define __builtin_va_end
+#define __DOXYGEN__
+#define __attribute__(x)
+#define NOINLINE __attribute__((noinline))
+#define prog_void
+#define PGM_VOID_P int
+            
+typedef unsigned char byte;
+extern "C" void __cxa_pure_virtual() {;}
+
+
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\mega\pins_arduino.h" 
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\MultiWii.ino"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Alarms.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Alarms.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\EEPROM.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\EEPROM.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\GPS.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\GPS.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\IMU.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\IMU.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\LCD.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\LCD.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\MultiWii.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\MultiWii.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Output.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Output.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Protocol.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Protocol.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\RX.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\RX.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Sensors.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Sensors.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Serial.cpp"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\Serial.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\config.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\def.h"
+#include "C:\Users\Dominic\Documents\GitHub\TytoRelayRemote\TytoRelayRemote\Firmware\types.h"
+#endif
