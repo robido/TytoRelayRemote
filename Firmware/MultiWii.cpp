853320dcb78b35a101f6ffa1d32b31a2c76047f7
Dominic Robillard <dominic@robipro.com>
2014-12-04 20:33:52 -0500

    Calibrated torque sensor

--- Firmware/MultiWii.cpp
+++ Firmware/MultiWii.cpp
@@ -397,7 +397,7 @@ void annexCode() { // this code is excetuted at each loop and won't interfere wi
   force = LPF_analog*analogRead(6)+(1.0-LPF_analog)*force;
   trust = LPF_analog*analogRead(7)+(1.0-LPF_analog)*trust;
   current = LPF_analog*analogRead(8)+(1.0-LPF_analog)*current;
-  Test_Jig_Data.Force_sensor = round(10.0*force);
+  Test_Jig_Data.Force_sensor = round(10.0*(1.272228871*force-1.526674645)); //0.1g resolution
   Test_Jig_Data.Trust_scale = round(10.0*(-0.2773562*trust+272.0864)); //0.1g resolution
   Test_Jig_Data.Current = round((current-512.0)*(5.0/(1023.0*0.000185))); //in mA
 
